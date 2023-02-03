4bee9785379425203b270fee42a2f0a663416772
Chloe <clo.gendreau@gmail.com>
2023-02-03 12:28:37 +0100

    create service

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,6 +1,6 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
-
+import { HttpClientModule} from '@angular/common/http';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { CoreModule } from './core/core.module';
@@ -12,7 +12,8 @@ import { CoreModule } from './core/core.module';
   imports: [
     BrowserModule,
     AppRoutingModule,
-    CoreModule
+    CoreModule,
+    HttpClientModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -1,9 +1,25 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
+import { Observable } from 'rxjs';
 
 @Injectable({
   providedIn: 'root'
 })
 export class TasksService {
 
-  constructor() { }
+  private collection$!: Observable<Task[]>;
+
+  constructor(private http: HttpClient) {
+    this.collection = this.http.get<Task[]>('http://localhost:3000/tasks');
+  }
+
+  //setter
+  public set collection(col: Observable<Task[]>) {
+    this.collection$ = col;
+  }
+
+  //getter
+  public get collection() : Observable<Task[]> {
+    return this.collection$;
+  }
 }
