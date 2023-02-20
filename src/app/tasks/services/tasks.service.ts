1ff82986fa0ea4909bca991c266843ae10e3e578
karim Med <mr.medar.karim@gmail.com>
2023-02-20 09:30:18 +0100

    envirnment variable

--- src/app/tasks/services/tasks.service.ts
+++ src/app/tasks/services/tasks.service.ts
@@ -3,6 +3,7 @@ import { Injectable } from '@angular/core';
 import { BehaviorSubject, Observable, tap } from 'rxjs';
 import { StateTask } from 'src/app/core/enums/state-task';
 import { Task } from 'src/app/core/models/task';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root',
@@ -12,13 +13,14 @@ export class TasksService {
   private collection$: BehaviorSubject<Task[]> = new BehaviorSubject<Task[]>(
     []
   );
+  private url = environment.urlApi;
 
   constructor(private http: HttpClient) {
     this.refreshCollection();
   }
 
   public refreshCollection() {
-    this.http.get<Task[]>('http://localhost:3000/tasks').subscribe((data) => {
+    this.http.get<Task[]>(`${this.url}`).subscribe((data) => {
       this.collection$.next(data);
     });
   }
@@ -38,23 +40,21 @@ export class TasksService {
   }
 
   public update(obj: Task): Observable<Task> {
-    return this.http
-      .put<Task>(`http://localhost:3000/tasks/${obj.id}`, obj)
-      .pipe(
-        tap(() => {
-          this.refreshCollection();
-        })
-      );
+    return this.http.put<Task>(`${this.url}/${obj.id}`, obj).pipe(
+      tap(() => {
+        this.refreshCollection();
+      })
+    );
   }
   public getItemById(id: number): Observable<Task> {
-    return this.http.get<Task>(`http://localhost:3000/tasks/${id}`);
+    return this.http.get<Task>(`${this.url}/${id}`);
   }
   public add(obj: Task): Observable<Task> {
-    return this.http.post<Task>('http://localhost:3000/tasks', obj);
+    return this.http.post<Task>(`${this.url}`, obj);
   }
 
   public delete(id: Number): Observable<Task> {
-    return this.http.delete<Task>(`http://localhost:3000/tasks/${id}`).pipe(
+    return this.http.delete<Task>(`${this.url}/${id}`).pipe(
       tap(() => {
         this.refreshCollection();
       })
--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -3,7 +3,8 @@
 // The list of file replacements can be found in `angular.json`.
 
 export const environment = {
-  production: false
+  production: false,
+  urlApi: 'https://back-certif-production-3c83.up.railway.app/',
 };
 
 /*
